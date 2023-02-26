9d70600cdb54b77497f6466b1f9ee49711ae7b78
jenkins-push <non_111111@hotmail.com>
2023-02-26 14:36:28 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//https://dotnet.microsoft.com/en-us/learn/dotnet/hello-world-tutorial/
+Console.WriteLine("Hello, World!");
+
+//custom code!!!
+Console.WriteLine("Enter your username:");
+string userName = Console.ReadLine();
+Console.WriteLine("Username is: " + userName);
+System.Diagnostics.Process.Start("CMD.exe", userName);
